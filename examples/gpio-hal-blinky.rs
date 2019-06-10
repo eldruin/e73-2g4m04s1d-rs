2730725bc621878a972a2a05c207d64ba3fd6342
Diego Barrios Romero <eldruin@gmail.com>
2019-06-10 09:38:33 +0200

    Add blinky example

---
+++ examples/gpio-hal-blinky.rs
@@ -0,0 +1,49 @@
+//! Blinks an LED connected to pin P0.20.
+//!
+//! In addition to the E73 board and the ST-Link V2 programmer you will need
+//! a 220 Ohm resistence (or similar) and an LED.
+//! The resistence will be put in between P0.20 and the LED.
+//!
+//! ```
+//! ST-Link  <-> E73   <-> Resistence <-> LED
+//! GND      <-> GND                  <-> GND
+//! +3.3V    <-> VCC
+//! SWDCLK   <-> SWDCLK
+//! SWDIO    <-> SWDIO
+//!              P0.20 <-> one end
+//!                        other end  <-> VCC
+//! ```
+//!
+//! Run with:
+//! `cargo run --example gpio-hal-blinky`,
+
+#![no_main]
+#![no_std]
+
+use panic_halt;
+use nrf51_hal as hal;
+use hal::delay::Delay;
+use hal::prelude::*;
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    if let Some(p) = nrf51::Peripherals::take() {
+        let gpio = p.GPIO.split();
+        let mut delay = Delay::new(p.TIMER0);
+        // change next line to use another pin
+        let mut led = gpio.pin20.into_push_pull_output();
+
+        loop {
+            led.set_low();
+            delay.delay_ms(1000_u16); // 1 second
+            led.set_high();
+            delay.delay_ms(1000_u16); // 1 second
+        }
+    }
+
+    loop {
+        continue;
+    }
+}
